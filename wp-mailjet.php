2885ff693c58f415ef10d8992f0a9d952d448dc2
Pavel Tashev <pavel@mailjet.com>
2014-12-09 14:07:44 +0200

    Translation of the iframe and corrections in i18n
    Translation of the iframe is not available because we send to the
    website an additional GET parameter called locale which sets the
    language of the iframe's content; Also small corrections are executed
    for the translations from i18n folder;

--- wp-mailjet.php
+++ wp-mailjet.php
@@ -2,7 +2,7 @@
 
 /*
 Plugin Name:	Mailjet for Wordpress
-Version:		3.1.15
+Version:		3.1.16
 Plugin URI:		https://www.mailjet.com/plugin/wordpress.htm
 Description:	Use mailjet SMTP to send email, manage lists and contacts within wordpress
 Author:			Mailjet SAS
