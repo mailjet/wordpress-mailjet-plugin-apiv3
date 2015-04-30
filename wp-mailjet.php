eda9e06edde0e8a7cc9e8940206d606ea48b4db2
FerhanGit <fismailov@mailjet.com>
2015-04-30 16:33:30 +0300

    * synchronized with the project at http://plugins.svn.wordpress.org/mailjet-for-wordpress

--- wp-mailjet.php
+++ wp-mailjet.php
@@ -2,7 +2,7 @@
 
 /*
 Plugin Name:	Mailjet for Wordpress
-Version:		3.1.19
+Version:		3.1.20
 Plugin URI:		https://www.mailjet.com/plugin/wordpress.htm
 Description:	Use mailjet SMTP to send email, manage lists and contacts within wordpress
 Author:			Mailjet SAS
