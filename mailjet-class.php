6a3b85bb2654c9e332216a83f05f354c7854b4d0
FerhanGit <fismailov@mailjet.com>
2015-06-23 16:33:17 +0300

    * https://redmine.mailjet.com/issues/2764 - removed Reply-To header since the client complains that adding "Reply-To" breaks his "Woocommerce" plugin

--- mailjet-class.php
+++ mailjet-class.php
@@ -76,34 +76,8 @@ class WP_Mailjet
 		$phpmailer->From = $from_email;
 		$phpmailer->Sender = $from_email;
 
-        // Add reply to header only if one does not already exist in the PhpMailer properties CustomerHeader and ReplyTo
-        // This check would work only for php >= 5.3
-        $hasReplyTo = $this->_getPhpMailerProperties($phpmailer, 'ReplyTo');
-        if (!$this->_customHeaderHasReplyTo($this->_getPhpMailerProperties($phpmailer, 'CustomHeader')) && empty($hasReplyTo)){
-            $phpmailer->addReplyTo(get_option('admin_email'));
-        }
 		$phpmailer->AddCustomHeader($this->api->mj_mailer);
 	}
-
-    private function _customHeaderHasReplyTo($propArr){
-        foreach($propArr as $prop){
-            if($prop[0] === 'Reply-To' && $prop[1] !== ''){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private function _getPhpMailerProperties(PHPMailer $phpmailer, $property)
-    {
-        $class = new ReflectionClass("PHPMailer");
-        $property = $class->getProperty($property);
-        if (method_exists($property, 'setAccessible')) {
-            $property->setAccessible(true);
-            return $property->getValue($phpmailer);
-        }
-        return array();
-    }
 
 	private function _get_auth_token()
 	{
