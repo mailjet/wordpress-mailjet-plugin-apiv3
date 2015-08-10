501cd50ffa5f830eeb924c8bd536d06e16047604
Mailjet <plugins@mailjet.com>
2015-08-10 16:09:39 +0300

    * Updated plugin version to 3.2.3

--- wp-mailjet.php
+++ wp-mailjet.php
@@ -2,7 +2,7 @@
 
 /*
 Plugin Name:	Mailjet for Wordpress
-Version:		3.2.2
+Version:		3.2.3
 Plugin URI:		https://www.mailjet.com/plugin/wordpress.htm
 Description:	Use mailjet SMTP to send email, manage lists and contacts within wordpress
 Author:			Mailjet SAS
