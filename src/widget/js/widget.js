41101a58dd081d30a9495dfb003fc49590d65057
Georgi Nikolov <gnikolov@mailjet.com>
2018-10-05 11:12:25 +0300

    Improve code style

--- src/widget/js/widget.js
+++ src/widget/js/widget.js
@@ -29,17 +29,14 @@
             var languageHiddenElementClass = getLanguageHiddenElements(this);
             $('#' + languageHiddenElementClass).toggle("slow");
 
-            var isSelected = $(this).prop('checked');
-            if(isSelected) {
-                // Require to select some option
-            }else {
+            // Uncheck the language checkbox
+            var removeLanguage = !$(this).prop('checked');
+            if(removeLanguage) {
                 // Reset Title and List ?
-                $(this).parent().find('.language-select-list').val(0);
                 $(this).parent().find('.title-input').val('');
+                $(this).parent().find('.language-select-list').val(0);
             }
 
-            // Add email addresses to:
-//            console.log($(this).parent().find('.language-select-list').val());
 
             // Hide advanced form, changes must be saved
 //            $("div#advanced-form-link-wrap").hide();
