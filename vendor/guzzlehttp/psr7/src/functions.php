f8bb4c3147d8ed9016e506c0b462d9717996aab7
Georgi Nikolov <gnikolov@mailjet.com>
2018-12-11 10:56:03 +0200

    Update composer.json autoload

--- vendor/guzzlehttp/psr7/src/functions.php
+++ vendor/guzzlehttp/psr7/src/functions.php
@@ -724,6 +724,7 @@ function mimetype_from_extension($extension)
         'txt' => 'text/plain',
         'wav' => 'audio/x-wav',
         'webm' => 'video/webm',
+        'webp' => 'image/webp',
         'wma' => 'audio/x-ms-wma',
         'wmv' => 'video/x-ms-wmv',
         'woff' => 'application/x-font-woff',
@@ -853,11 +854,16 @@ function get_message_body_summary(MessageInterface $message, $truncateAt = 120)
 {
     $body = $message->getBody();
 
-    if (!$body->isSeekable()) {
+    if (!$body->isSeekable() || !$body->isReadable()) {
         return null;
     }
 
     $size = $body->getSize();
+
+    if ($size === 0) {
+        return null;
+    }
+
     $summary = $body->read($truncateAt);
     $body->rewind();
 
