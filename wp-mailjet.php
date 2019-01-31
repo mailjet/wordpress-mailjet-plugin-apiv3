ac320c43a0d8be2331c398ccc2c0f75db1c67c50
Georgi Nikolov <gnikolov@mailjet.com>
2019-01-31 17:49:47 +0200

    Add php min requirement

--- wp-mailjet.php
+++ wp-mailjet.php
@@ -46,48 +46,18 @@ if (!defined('WPINC')) {
 // Autoloading via composer
 require_once __DIR__ . '/vendor/autoload.php';
 
-use Analog\Analog;
+//use Analog\Analog;
 use MailjetPlugin\Includes\Mailjet;
-use MailjetPlugin\Includes\MailjetActivator;
-use MailjetPlugin\Includes\MailjetDeactivator;
-use MailjetPlugin\Includes\MailjetSettings;
 use MailjetPlugin\Includes\MailjetUpdate;
 
-// Change the handler to any other if you need to.
-//Analog::handler(\Analog\Handler\File::init(dirname(__FILE__) . '/logs.txt'));
-Analog::handler(\Analog\Handler\ChromeLogger::init());
-
 /**
- * Currently plugin version.
+ * Mailjet plugin version.
  */
 define('MAILJET_VERSION', '5.0.7');
 
 
 // Call the update to V5 logic
 MailjetUpdate::updateToV5();
-
-
-
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/MailjetActivator.php
- */
-function activate_mailjet()
-{
-    MailjetActivator::activate();
-}
-
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/MailjetDeactivator.php
- */
-function deactivate_mailjet()
-{
-    MailjetDeactivator::deactivate();
-}
-
-//register_activation_hook(__FILE__, 'activate_mailjet');
-//register_deactivation_hook(__FILE__, 'deactivate_mailjet');
 
 /**
  * Begins execution of the plugin.
@@ -104,4 +74,73 @@ function run_mailjet()
     $plugin->run();
 }
 
+
+$activator = new MailjetActivator();
+register_activation_hook( __FILE__, array( $activator, 'activation_check' ) );
+
 run_mailjet();
+
+/**
+ * Triggered on mailjet plugin activation
+ * Check if server php version is higher enough
+ */
+class MailjetActivator
+{
+
+    function __construct()
+    {
+        add_action('admin_init', array($this, 'check_version'));
+
+        // Don't run anything else in the plugin, if we're on an incompatible WordPress version
+        if (!self::compatible_version()) {
+            return;
+        }
+    }
+
+    // The primary sanity check, automatically disable the plugin on activation if it doesn't
+    // meet minimum requirements.
+    function activation_check()
+    {
+        if (!self::compatible_version()) {
+            deactivate_plugins(plugin_basename(__FILE__));
+            $phpVersion = phpversion();
+            $message = sprintf(esc_html__('Mailjet for WordPress requires PHP 5.5 or later. Your server currently runs on PHP %s. Please upgrade your PHP and activate the plugin again.', 'wp-mailjet'), $phpVersion);
+            wp_die($message);
+        }
+    }
+
+    // The backup sanity check, in case the plugin is activated in a weird way,
+    // or the versions change after activation.
+    function check_version()
+    {
+        if (!self::compatible_version()) {
+            if (is_plugin_active(plugin_basename(__FILE__))) {
+                deactivate_plugins(plugin_basename(__FILE__));
+                add_action('admin_notices', array($this, 'disabled_notice'));
+                if (isset($_GET['activate'])) {
+                    unset($_GET['activate']);
+                }
+            }
+        }
+    }
+
+    function disabled_notice()
+    {
+        $phpVersion = phpversion();
+        $message = sprintf(esc_html__('Mailjet for WordPress requires PHP 5.5 or later. Your server currently runs on PHP %s. Please upgrade your PHP and activate the plugin again.', 'wp-mailjet'), $phpVersion);
+        echo '<strong>' . $message . '</strong>';
+    }
+
+    static function compatible_version()
+    {
+        $phpVersion = phpversion();
+        if (version_compare($phpVersion, '5.5', '<')) {
+            return false;
+        }
+
+        // Add sanity checks for other version requirements here
+
+        return true;
+    }
+
+}
