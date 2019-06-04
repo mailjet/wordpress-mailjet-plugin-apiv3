6d4659c6c8a4ef0253e5f235a89522b009900752
Yasen Angelov <yangelov@mailjet.com>
2019-06-04 11:11:29 +0300

    no message to the front

--- src/widget/WP_Mailjet_Subscribe_Widget.php
+++ src/widget/WP_Mailjet_Subscribe_Widget.php
@@ -890,7 +890,7 @@ class WP_Mailjet_Subscribe_Widget extends \WP_Widget
 
         $validApiCredentials = MailjetApi::isValidAPICredentials();
         if ($validApiCredentials === false) {
-            include(plugin_dir_path(__FILE__) . 'views' . DIRECTORY_SEPARATOR . 'designforfailure.php');
+//            include(plugin_dir_path(__FILE__) . 'views' . DIRECTORY_SEPARATOR . 'designforfailure.php');
             return false;
         }
 
