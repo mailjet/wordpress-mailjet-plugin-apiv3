1c448fae0e79e4cd7e0f267a314a2d71d1560cd0
Yasen Angelov <yangelov@mailjet.com>
2019-06-13 16:16:17 +0300

    Changelist rework

--- src/templates/admin/settingTemplates/SubscriptionSettingsPartials/subscrFieldset.php
+++ src/templates/admin/settingTemplates/SubscriptionSettingsPartials/subscrFieldset.php
@@ -4,6 +4,8 @@ $mailjetContactLists = get_query_var('mailjetContactLists');
 $mailjetCommentAuthorsList = get_query_var('mailjetCommentAuthorsList');
 $mailjetInitialSyncActivated = get_query_var('mailjetInitialSyncActivated');
 $mailjetCommentAuthorsSyncActivated = get_query_var('mailjetCommentAuthorsSyncActivated');
+$resyncBtn = $mailjetSyncActivated == 1 ? '&nbsp&nbsp<a href="#" onclick="ajaxResync()">Resync</a>' : '';
+$changeList = $mailjetContactLists === 'No list selected' ? 'Select List' : 'Change List';
 ?>
 
 <fieldset class="settingsSubscrFldset">
@@ -12,11 +14,11 @@ $mailjetCommentAuthorsSyncActivated = get_query_var('mailjetCommentAuthorsSyncAc
 		<span><?php _e('Automatically add Wordpress users to a Mailjet list. Each user’s email address and role (subscriber, administrator, author, …) is synchronized to the list and available for use inside Mailjet.', 'mailjet-for-wordpress'); ?></span>
 	</label>
 
-	<div id="activate_mailjet_sync_form" class="<?=($mailjetSyncActivated == 1 ? ' mj-show' : 'mj-hide') ?>">
+	<div id="activate_mailjet_sync_form" class="mj-show">
 		<div class="mailjet_sync_options_div">
 			<div id="contact_list" class="mailjet_sync_woo_div">
 				<div class="mj-woocommerce-contacts" id="div-for-ajax">
-					<?= $mailjetContactLists ?>  <span>&nbsp&nbsp<a href="#" onclick="ajaxResync()">Resync</a>&nbsp&nbsp<a href="#" onclick="loadLists()">Change list</a></span>
+					<?= $mailjetContactLists ?>  <span><?= $resyncBtn?>&nbsp&nbsp<a href="#" onclick="loadLists()"><?= $changeList?></a></span>
 				</div>
 			</div>
 		</div>
