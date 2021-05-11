de0bf94faff99aa2a2325b1a877fd63e8f4cd5b1
Artem Bondarenko <abonda@rainpos.com>
2021-05-11 20:02:11 +0300

    5.2.7 release

--- src/includes/Mailjet.php
+++ src/includes/Mailjet.php
@@ -67,7 +67,7 @@ class Mailjet
         if (defined('MAILJET_VERSION')) {
             $this->version = MAILJET_VERSION;
         } else {
-            $this->version = '5.2.6';
+            $this->version = '5.2.7';
         }
         $this->plugin_name = 'mailjet';
 
