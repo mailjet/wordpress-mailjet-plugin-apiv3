5a2db902b70c55844f4c0f6f729c45592c4003da
Alexander Mykhailenko <mihaskep@gmail.com>
2022-07-20 02:33:16 +0300

    Plugin fixes

--- src/widget/views/designforfailure.php
+++ src/widget/views/designforfailure.php
@@ -1,6 +1,5 @@
 <p>
-    <?php 
-namespace MailjetWp;
+    <?php
 
 if ($validApiCredentials == \false) {
     _e('Please make sure that you are using the correct API key and Secret key associated to your Mailjet account: <a href="https://app.mailjet.com/account/api_keys" target="_blank">https://app.mailjet.com/account/api_keys</a>', 'mailjet-for-wordpress');
