874dcb0fcd6872be1060e44a338d991468fe6a0d
Oleksandr Mykhailenko <mihaskep@gmail.com>
2023-05-15 22:03:15 +0300

    Release new version 5.4.5
    ## Changes
    - Fixed bug with activating Woocommerce email notification templates.

--- src/includes/Mailjet.php
+++ src/includes/Mailjet.php
@@ -62,7 +62,7 @@ class Mailjet
         if (\defined('MAILJET_VERSION')) {
             $this->version = MAILJET_VERSION;
         } else {
-            $this->version = '5.4.4';
+            $this->version = '5.4.5';
         }
         $this->plugin_name = 'mailjet';
         $this->load_dependencies();
