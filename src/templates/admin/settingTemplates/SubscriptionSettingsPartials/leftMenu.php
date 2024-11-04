8dab42351fe708a2bcf766adb51a32842d04a706
Oleksandr Mykhailenko <mihaskep@gmail.com>
2024-11-04 14:17:44 +0200

    Branding update

--- src/templates/admin/settingTemplates/SubscriptionSettingsPartials/leftMenu.php
+++ src/templates/admin/settingTemplates/SubscriptionSettingsPartials/leftMenu.php
@@ -10,23 +10,17 @@ $currentPage = get_query_var('currentPage');
             <?php
             echo '<a data-img_id="settingsMenuLinkImg1" class="' . esc_attr(($currentPage === 'mailjet_connect_account_page' ? 'active' : '')) . '" href="admin.php?page=mailjet_connect_account_page">';
             ?>
-            <svg class="settingsMenuLinkImg1" width="16" viewBox="0 0 16 16">
-                <defs>
-                    <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="b">
-                        <stop stop-color="#FFBC48" offset="0%"/>
-                        <stop stop-color="#FFA414" offset="100%"/>
-                    </linearGradient>
-                    <rect id="a" width="16" height="16" rx="3"/>
-                </defs>
-                <g fill-rule="nonzero" fill="none">
-                    <g>
-                        <use fill="#D8D8D8" xlink:href="#a"/>
-                        <use fill="url(#b)" xlink:href="#a"/>
-                    </g>
-                    <path class="settingsMenuLinkImg1"
-                          d="M6.518 7.887l-.183 1.271-1.322 1.911.437-.244 7.208-3.965L14 6.118l-7.482 1.77zm-.051-.335l5.652-1.281-.366-.051-1.484-.203-3.152-.438L3 5l1.515 1.108 1.9 1.393.052.05z"
-                          fill="#FFFFFF"/>
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_2_114)">
+                    <path d="M13 0H3C1.34315 0 0 1.34315 0 3V13C0 14.6569 1.34315 16 3 16H13C14.6569 16 16 14.6569 16 13V3C16 1.34315 14.6569 0 13 0Z" fill="#D8D8D8"/>
+                    <path d="M13 0H3C1.34315 0 0 1.34315 0 3V13C0 14.6569 1.34315 16 3 16H13C14.6569 16 16 14.6569 16 13V3C16 1.34315 14.6569 0 13 0Z" fill="#9585F4"/>
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M2 7.12457L6.01364 8.94384L6.82012 8.14205L4.76641 7.20897L11.1338 4.77078L8.68627 11.1101L7.75789 9.07513L6.94672 9.88161L6.98892 9.97538L8.76598 13.8718L13 2.914L2 7.12457Z" fill="white"/>
                 </g>
+                <defs>
+                    <clipPath id="clip0_2_114">
+                        <rect width="16" height="16" fill="white"/>
+                    </clipPath>
+                </defs>
             </svg>
             <span><?php
                 echo esc_textarea(__('Connect your Mailjet account', 'mailjet-for-wordpress'));
